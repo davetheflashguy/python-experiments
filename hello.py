8c7027596a2f30b813f5c3283aa19cac66910ce1
Dave Collier <davetheflashguy@gmail.com>
2013-11-27 09:11:19 -0500

    Messing around with strings

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 import sys
 
 def main():
-	print "hello there", sys.argv[1]
+	username = sys.argv[1]
+	vowels = ["a", "e", "i", "o", "u"]
+	print "username", username
+	print "has " + str(len(username)) + " characters"
+	print "username starts with the letter " + username[0] + " "
+	print "and ends with the letter " + username[len(username) - 1]
 
 
 if __name__ == '__main__':
